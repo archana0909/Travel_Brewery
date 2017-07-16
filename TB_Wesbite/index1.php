2ce6ef12764b4a014b189cf07b51a08e8a1afe84
Archana <archana_0909@hotmail.com>
2017-07-16 14:36:58 +0530

    footer implementation ver 1
    improvements pending
    
    -re-alignment of 1st column on small screens

---
+++ TB_Wesbite/index1.php
@@ -0,0 +1,315 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="description" content="Creating custom Europe tours for backpackers and budget tourists and group tours" />
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="author" content="Le Pension Group">
+  <title>Travel Brewery </title>
+  <!-- jQuery -->
+  <!-- Bootstrap Core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom Fonts -->
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:400,600" rel="stylesheet" type="text/css">
+  <!-- Plugin CSS -->
+  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <!-- Theme CSS -->
+  <link href="css/creative.css" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+    <?php include 'header.php';?>
+    <div class="header-content">
+      <div class="header-content-inner">
+        <h1 id="homeHeading">Brewing Custom Tours Only For You</h1>
+        <hr>
+        <p><h3>Solo and Group Backpacking tours across Eastern and Central Europe</h3></p>
+        <a href="#about" class="btn btn-primary btn-xl page-scroll">Find Out More</a>
+      </div>
+    </div>
+  </header>
+  <section class="bg-primary" id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+          <h2 class="section-heading text-center">Brewing Custom Tours With Love
+            <i class="fa fa-heart text-primary heart-icon"></i>
+          </h2>
+          <hr class="light">
+          <p class="text-faded">Planning and implementing an interesting and fulfilling trip is much like brewing a
+            fine drink. Every step requires patience and careful attention, if the process is not followed well,
+            the end result can often be quite underwhelming, leaving a bad after taste.
+            <br/><br/>
+            At The Travel Brewery, we put great stress on various stages of trip planning,
+            from conceptualizing to choosing the best possible itinerary and finally to the execution.
+            The effort is to offer something unique where in you get to explore the beautiful European
+            sights just like a local.
+            <br/><br/>
+            No more boring schedules and fixed itineraries, we offer you affordable
+            European vacations that give you unlimited freedom.
+            <p><h2 class="text-center">#travellikealocal</h2></p>
+          </p>
+        </div>
+      </div>
+      <div class="icon-aboutus row">
+        <div class="col-lg-4 text-center">
+          <img class="img-icon" src="img/icons/snorkel.png">
+          <h4>EXPLORE</h4>
+        </div>
+        <div class="col-lg-4 text-center">
+          <img class="img-icon" src="img/icons/balloon.png">
+          <h4>EXPERIENCE</h4>
+        </div>
+        <div class="col-lg-4 text-center">
+          <img class="img-icon" src="img/icons/drink.png">
+          <h4>ENJOY</h4>
+        </div>
+
+      </div>
+      <div class="text-center custom-padding">
+        <a href="#services" class="page-scroll btn btn-default ">
+          <i class="fa fa-2x fa-chevron-down  text-primary sr-icons"></i>
+        </a>
+      </div>
+    </div>
+  </section>
+  <section id="services">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 text-center">
+          <h2 class="section-heading"> What's Included in your itinerary </h2>
+          <hr>
+        </div>
+      </div>
+      <div class="icon-aboutus row">
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="service-box">
+            <i class="fa fa-4x fa-hotel text-primary sr-icons"></i>
+            <h3>Accomodation</h3>
+            <p class="text-muted">We find the best places to stay at central locations with great facilities</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="service-box">
+            <i class="fa fa-4x fa-train text-primary sr-icons"></i>
+            <h3>Logistics</h3>
+            <p class="text-muted">We take care of your inter city and intra city travel,
+              making it easier for you to explore each city</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-group text-primary sr-icons"></i>
+              <h3>Activities</h3>
+              <p class="text-muted">Enjoy an in depth walking tour during the day,<br/>go for amazing pub crawls at night !!!</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-qq text-primary sr-icons"></i>
+              <h3>Trip Assistance</h3>
+              <p class="text-muted">Ask us anything - before, during and after the trip.<br/>
+                Our in house expert will travel along you on backpacking trips.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="tours">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 text-center">
+              <h2 class="section-heading">UPCOMING TRIPS</h2>
+              <h3 class="section-heading"><a href="eastern-europe-trip-details.php">THE EAST EUROPEAN SOJOURN</a></h3>
+              <hr class="primary">
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3 col-md-6 text-center">
+              <div class="service-box">
+                <i class="fa fa-4x fa-globe text-primary sr-icons"></i>
+                <h3>5 Countries</h3>
+                <!-- <p class="text-muted"></p> -->
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 text-center">
+              <div class="service-box">
+                <i class="fa fa-4x fa-building text-primary sr-icons"></i>
+                <i class="fa fa-3x fa-building text-primary sr-icons"></i>
+
+                <h3>5 Cities</h3>
+                <!-- <p class="text-muted">We update dependencies to keep things fresh.</p> -->
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 text-center">
+              <div class="service-box">
+                <i class="fa fa-4x fa-calendar text-primary sr-icons"></i>
+                <h3>12 Nights</h3>
+                <!-- <p class="text-muted">You have to make your websites with love these days!</p> -->
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 text-center">
+              <div class="service-box">
+                <i class="fa fa-2x fa-child text-primary sr-icons"></i>
+                <i class="fa fa-4x fa-child text-primary sr-icons"></i>
+                <i class="fa fa-2x fa-child text-primary sr-icons"></i>
+
+                <h3>Unlimited Fun</h3>
+                <!-- <p class="text-muted">You can use this theme as is, or you can make changes!</p> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="no-padding" id="Citieslist">
+        <div class="container-fluid">
+          <div class="row no-gutter popup-gallery">
+            <div class="col-lg-4 col-sm-6 portfolio-box">
+              <img src="img/cityimages/tn_berlin_1.jpg" class="img-responsive" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    BERLIN
+                  </div>
+                  <div class="project-name">
+                    A city where every monument tells a story, Berlin takes you back in time.
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6 portfolio-box">
+
+            <img src="img/cityimages/tn_budapest_1.jpg" class="img-responsive" alt="">
+            <div class="portfolio-box-caption">
+              <div class="portfolio-box-caption-content">
+                <div class="project-category text-faded">
+                  BUDAPEST
+                </div>
+                <div class="project-name">
+                  A Backpacker's delight with a beautiful river front, legendary Ruin Bars and 15th century Turkish Baths
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-box">
+          <img src="img/cityimages/tn_prague_1.jpg" class="img-responsive" alt="">
+          <div class="portfolio-box-caption">
+            <div class="portfolio-box-caption-content">
+              <div class="project-category text-faded">
+                PRAGUE
+              </div>
+              <div class="project-name">
+                The heart of Europe, soak in the beauty  of Prague with a Chilled Beer :D
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-box">
+
+        <img src="img/cityimages/tn_bratislava_1.jpg" class="img-responsive" alt="">
+        <div class="portfolio-box-caption">
+          <div class="portfolio-box-caption-content">
+            <div class="project-category text-faded">
+              BRATISLAVA
+            </div>
+            <div class="project-name">
+              Dine like a royal in the Old Town Center
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-lg-4 col-sm-6 portfolio-box">
+
+      <img src="img/cityimages/tn_vienna_1.jpg" class="img-responsive" alt="">
+      <div class="portfolio-box-caption">
+        <div class="portfolio-box-caption-content">
+          <div class="project-category text-faded">
+            VIENNA
+          </div>
+          <div class="project-name">
+            The European epicenter of arts, music and culture
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+  <div class="col-lg-4 col-sm-6 portfolio-box">
+
+    <img src="img/cityimages/tn_india_1.jpg" class="img-responsive" alt="">
+    <div class=" portfolio-box-caption">
+      <div class="portfolio-box-caption-content">
+        <div class="project-category text-faded">
+          HOME
+        </div>
+        <div class="project-name">
+          Home is where ur heart is
+        </div>
+      </div>
+    </div>
+  </a>
+</div>
+</div>
+</div>
+</section>
+
+<aside class="bg-dark">
+  <div class="container text-center">
+    <div class="call-to-action">
+      <h2>Let's Get Started!</h2>
+      <hr class="primary">
+      <p>Explore and experience Eastern and Central Europe like a local. Enjoy Affordable European Vacations
+        at unbelievable prices.</p> <p>Don't believe us? Check out the trip details. Limited slots on offer!</p>
+        <a href="eastern-europe-trip-details.php" class="btn btn-default btn-xl sr-button">Trip Details</a>
+      </div>
+    </div>
+  </aside>
+
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 text-center">
+          <h2 class="section-heading">Contact Us</h2>
+          <hr class="primary">
+        </div>
+        <div class="col-lg-4 col-lg-offset-2 text-center">
+          <i class="fa fa-phone fa-3x sr-contact"></i>
+          <p>+91-8452962313</p>
+          <p>+91-7702303908</p>
+        </div>
+        <div class="col-lg-4 text-center">
+          <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+          <p><a href="mailto:info@travelbrewery.com">info@travelbrewery.com</a></p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <?php include 'footer.php';?>
+
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <!-- Bootstrap Core JavaScript -->
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <!-- Plugin JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+  <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Theme JavaScript -->
+  <script src="js/creative.js"></script>
+</body>
+</html>
